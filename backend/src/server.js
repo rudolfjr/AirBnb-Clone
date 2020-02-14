72fd1c36efbc40e02bb28da411dfca6e3789f9ad
Rudolf Jr <rudinhok@gmail.com>
2020-02-13 22:19:14 -0300

    Initial Commit

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://rudinhok:rudinhok@omnistack-a3eqz.mongodb.net/aircnc?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+app.listen(3333);
