ba92ce560e1d0687ece22102d908e452cfb369f7
Rudolf Jr <rudinhok@gmail.com>
2020-02-25 15:42:29 -0300

    Adicionando Realtime, para a aplicação e para o APP finalizando a aplicação

--- backend/src/controllers/ApprovalController.js
+++ backend/src/controllers/ApprovalController.js
@@ -9,6 +9,12 @@ module.exports = {
 
         await booking.save();
 
+        const bookingUserSocket = req.connectedUsers[booking.user];
+
+        if(bookingUserSocket){
+            req.io.to(bookingUserSocket).emit('booking_response', booking);
+        }
+
         return res.json(booking);
     }
 };
